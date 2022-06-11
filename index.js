b8127dd7cd859675a09957eac5f882c0f735ad1c
Terormumuli <terahmumuli@gmail.com>
2022-06-11 22:32:57 +0300

    Intro to Arrays

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(){
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat(){
+    cats.pop('Ralph')
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift('Milo')
+}
+const appendCat = () => {
+    let cats1 = [...cats];
+    cats1.push('Broom')
+    return cats1;
+}
+
+const prependCat = () => {
+    let cats2 = [...cats];
+        cats2.unshift('Arnold');
+        return cats2;
+}
+
+const removeLastCat = () => {
+    let cats1 = [...cats];
+    cats1.splice(cats.length-1);
+    return cats1;
+}
+const removeFirstCat = () => {
+    let cats1 = [...cats];
+    cats1.splice(0,1);
+    return cats1;
+}
